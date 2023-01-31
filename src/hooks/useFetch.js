60fe7e8470cdb3f9a39cb041a1ee07a15516c8cd
Frost <svet.tyshchyk@gmail.com>
2023-01-31 20:07:00 +0200

    feat: add page pagination

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,17 +2,18 @@ import React from "react";
 import {useGetGoodsQuery} from "../storage/goodsApi";
 
 export default function useFetch(url, limit, skip = 0) {
-    const { data, error, isLoading } = useGetGoodsQuery({
-        url,
-        limit,
-        skip
-    });
     const [fetchData, setFetchData] = React.useState({
         url: url,
         limit: limit,
         skip: skip,
     })
-    const [pagesCount, setPagesCount] = React.useState(1)
+    const { data, error, isLoading } = useGetGoodsQuery({
+        url: fetchData.url,
+        limit: fetchData.limit,
+        skip: fetchData.skip
+    });
+
+    const [pagesCount, setPagesCount] = React.useState(10)
 
 
     return {data, isLoading, fetchData, error, setFetchData, pagesCount}
